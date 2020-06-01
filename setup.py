06f1ad34a8ddac8ec63191e4547c7a18fef86bed
alexsaplin <saplin.aa@phystech.edu>
2020-06-01 22:51:46 +0300

    DeepMux API v0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="deepmux",
-    version="0.1",
+    version="0.2.0",
     author="DeepMux",
     author_email="dev@deepmux.com",
     long_description=long_description,
